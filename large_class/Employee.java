328a8e5df6b9c7f8095bf449ed48442a197f0ace
1052986 <s9352816@gmail.com>
2019-05-25 15:55:49 +0800

    refactory large class

--- large_class/Employee.java
+++ large_class/Employee.java
@@ -3,26 +3,49 @@ package com.directi.training.codesmells.largeclass;
 public class Employee
 {
     private final String _name;
-    private final String _officeAreaCode;
-    private final String _officeNumber;
-    private final String _officeExtensionNumber;
+    private final Employee_officephone _telephone;
+    //private final String _officeAreaCode;
+    //private final String _officeNumber;
+    //private final String _officeExtensionNumber;
 
-    public Employee(String name, String officeAreaCode, String officeNumber,
-                    String officeExtensionNumber)
+    public Employee(String name, Employee_officephone phone)
     {
         _name = name;
-        _officeAreaCode = officeAreaCode;
-        _officeNumber = officeNumber;
-        _officeExtensionNumber = officeExtensionNumber;
+	_telephone = phone
+        //_officeAreaCode = officeAreaCode;
+        //_officeNumber = officeNumber;
+        //_officeExtensionNumber = officeExtensionNumber;
     }
 
-    public String getTelephoneNumber()
+    public Employee_officephone getTelephoneNumber()
     {
-        return "(" + _officeAreaCode + ") " + _officeNumber + "-" + _officeExtensionNumber;
+        return _telephone;
     }
 
     public String getName()
     {
         return _name;
     }
+}
+
+
+public class Employee_officephone
+{
+    private final String _officeAreaCode;
+    private final String _officeNumber;
+    private final String _officeExtensionNumber;
+
+    public Employee_officephone(String officeAreaCode, String officeNumber,
+                    String officeExtensionNumber)
+    {
+         _officeAreaCode = officeAreaCode;
+         _officeNumber = officeNumber;
+         _officeExtensionNumber = officeExtensionNumber;
+    }
+
+    public String toString()
+    {
+        return "(" + _officeAreaCode + ") " + _officeNumber + "-" + _officeExtensionNumber;
+    }
+
 }
