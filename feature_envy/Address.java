ef94d90460181942793d18d122b8c0d4bd673b75
1052986 <s9352816@gmail.com>
2019-05-25 16:12:34 +0800

    refactory customer,address

--- feature_envy/Address.java
+++ feature_envy/Address.java
@@ -50,4 +50,12 @@ public class Address
     {
         return _postalCode;
     }
-}
+
+    public void print()
+    {
+	System.out.println(
+			_addressLine1 + "\n" + _addressLine2 + "\n" + 
+			_city + ", " + _state + "\n" +
+			_postalCode);
+
+    }
